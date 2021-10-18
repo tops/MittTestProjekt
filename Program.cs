2ed76b9a68961a54ec81eaaf72395ef912704411
Tobias Landén <tobias.landen@gmail.com>
2021-10-18 13:40:27 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MittTestProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
